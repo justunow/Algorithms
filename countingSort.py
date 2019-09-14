4add3353851cd769085808a64b37c48dc19e3813
justunow <36323764+justunow@users.noreply.github.com>
2019-09-14 15:44:29 -0500

    Add files via upload

--- countingSort.py
+++ countingSort.py
@@ -7,18 +7,16 @@ for i in range(number):
 
 def countingSort(nums):
     n = len(nums)
-    output = [0] * n
     count = [0] * 10
-    for i in range(0, n):
+    output = [0] * n
+    for i in range(n):
         count[nums[i]] += 1
     for i in range(1, 10):
         count[i] += count[i - 1]
-    i = n - 1
-    while i >= 0:
+    for i in range(n - 1, -1, -1):
         output[count[nums[i]] - 1] = nums[i]
         count[nums[i]] -= 1
-        i -= 1
-    for i in range(0, n):
+    for i in range(n):
         nums[i] = output[i]
 
 
